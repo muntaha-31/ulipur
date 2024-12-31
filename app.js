0cc8e8760fcc9e1e3aa5f733374d488b572aea16
muntaha-31 <webmuntahar@gmail.com>
2024-12-31 15:07:54 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let body = document.getElementById("bdy")
+let btn_red = () => {
+    body.style.backgroundColor='red'
+}
+let btn_yellow = () => {
+    body.style.backgroundColor='yellow'
+}
+let btn_green = () => {
+    body.style.backgroundColor='green'
+}
